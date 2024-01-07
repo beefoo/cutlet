b49aedaf3ef680df4be9fd22d56a3c332b842fef
beefoo <brian@youaremyjoy.org>
2024-01-06 22:09:01 -0500

    ignore segments along the edge, define a minimum segment size

--- scripts/segment_images.py
+++ scripts/segment_images.py
@@ -28,7 +28,7 @@ def parse_args():
         help="Path to checkpoint file",
     )
     parser.add_argument(
-        "-segments", dest="MAX_SEGMENTS", default=8, help="Max segments per image"
+        "-segments", dest="MAX_SEGMENTS", default=3, help="Max segments per image"
     )
     parser.add_argument(
         "-out",
@@ -72,29 +72,29 @@ def main(a):
         empty_directory(a.OUTPUT_DIR)
 
     filenames = get_filenames(a.INPUT_FILES)
-    fileCount = len(filenames)
-    print(f"{fileCount} files found.")
+    file_count = len(filenames)
+    print(f"{file_count} files found.")
 
     sam = sam_model_registry[a.MODEL](checkpoint=a.MODEL_CHECKPOINT)
     if torch.cuda.is_available():
         print("CUDA is available")
         sam.to(device="cuda")
-    mask_generator = SamAutomaticMaskGenerator(sam)
+    mask_generator = SamAutomaticMaskGenerator(sam, min_mask_region_area=(32 * 32))
 
     for i, fn in enumerate(filenames):
         existsFn = f"{a.OUTPUT_DIR}/{get_basename(fn)}-1.png"
         if os.path.isfile(existsFn):
-            print(f"Already processed {i+1} of {fileCount}: {fn}")
+            print(f"Already processed {i+1} of {file_count}: {fn}")
             continue
-        print(f"Processing {i+1} of {fileCount}: {fn}")
+        print(f"Processing {i+1} of {file_count}: {fn}")
         image = cv2.imread(fn)
-        imH, imW, imC = image.shape
-        imD = max(imH, imW)
-        if imD > a.MAX_IMAGE_DIMENSION:
+        im_h, im_w, im_c = image.shape
+        im_d = max(im_h, im_w)
+        if im_d > a.MAX_IMAGE_DIMENSION:
             scale = 1.0 * a.MAX_IMAGE_DIMENSION / imD
-            imH = round_int(imH * scale)
-            imW = round_int(imW * scale)
-            image = cv2.resize(image, (imW, imH))
+            im_h = round_int(im_h * scale)
+            im_w = round_int(im_w * scale)
+            image = cv2.resize(image, (im_w, im_h))
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         try:
             masks = mask_generator.generate(image)
@@ -102,30 +102,41 @@ def main(a):
             print(f"Error with file {fn}; skipping: {error}")
             continue
         masks = sorted(masks, key=(lambda x: x["area"]), reverse=True)
-        sampleSize = a.MAX_SEGMENTS
-        if len(masks) > sampleSize:
-            masks = masks[:sampleSize]
+        # remove masks that are on the edge
+        non_edge_masks = []
+        for mask in masks:
+            edge = max(10, im_h * 0.05, im_w * 0.05)
+            x, y, w, h = tuple(mask["bbox"])
+            x2 = x + w
+            y2 = y + h
+            if x > edge and y > edge and x2 < (im_w - edge) and y2 < (im_h - edge):
+                non_edge_masks.append(mask)
+        masks = non_edge_masks
+        sample_size = a.MAX_SEGMENTS
+        if len(masks) > sample_size:
+            masks = masks[:sample_size]
         if len(masks) == 0:
             continue
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGRA)  # add transparency
         for j, mask in enumerate(masks):
-            segmentFn = f"{a.OUTPUT_DIR}/{get_basename(fn)}-{j+1}.png"
-            bbox = mask["bbox"]
-            x, y, w, h = tuple(bbox)
+            segment_fn = f"{a.OUTPUT_DIR}/{get_basename(fn)}-{j+1}.png"
+            x, y, w, h = tuple(mask["bbox"])
             if x is None or y is None or w is None or h is None:
                 continue
             x = int(x)
             y = int(y)
             w = int(w)
             h = int(h)
+            x2 = x + w
+            y2 = y + h
             segment = mask["segmentation"]
-            # print(bbox, segment.shape, segment.dtype)
+            # print(mask["bbox"], segment.shape, segment.dtype)
             # bg = np.zeros(image.shape, image.dtype)
             segment_mask = segment.astype(np.uint8)
             segment_mask *= 255
             masked_image = cv2.bitwise_and(image, image, mask=segment_mask)
-            cropped_image = masked_image[y : y + h, x : x + w]
-            cv2.imwrite(segmentFn, cropped_image)
+            cropped_image = masked_image[y:y2, x:x2]
+            cv2.imwrite(segment_fn, cropped_image)
 
 
 main(parse_args())
